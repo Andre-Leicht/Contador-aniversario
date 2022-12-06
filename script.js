27a51329f2ed09f1ef91b7d89ce7e6c675fbc617
André Vinícius Leicht <andreerdnaleicht@gmail.com>
2022-12-06 16:31:13 -0300

    reseta sozinho

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const daysEl = document.getElementById("days");
 const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
+const dateOfBirthDay = document.querySelector("#birthday");
 
-const newYears = "28 Oct 2022";
+let year = new Date()
+let newDate = "28 Oct "+ (year.getFullYear()+1) ;
+let birthday = (year.getFullYear()+1) - 2002;
 
 function countdown(){
-    const newYerasDate = new Date(newYears);
+
+    const newYerasDate = new Date(newDate);
     const currentDate = new Date();
 
     const totalSeconds = (newYerasDate - currentDate) / 1000;
@@ -15,11 +19,13 @@ function countdown(){
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
+    
 
     daysEl.innerHTML = days;
     hoursEl.innerHTML = formatTime(hours);
     minutesEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
+    dateOfBirthDay.innerHTML = birthday + " anos em";
 
     function formatTime(time){
         return time < 10 ? `0${time}` : time;
